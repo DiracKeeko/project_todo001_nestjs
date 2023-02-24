71ed835e862713cce051e5c7d2e610bcde861094
Keeko <787018661@qq.com>
2023-02-24 17:23:36 +0800

    [feat] app.setGlobalPrefix('api')

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api'); // 全局添加 "api"前缀
+  await app.listen(6006);
 }
 bootstrap();
