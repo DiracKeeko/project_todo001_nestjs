ac2e6dc33f1fd1c8705ed806908b45acd03fddc7
keeko <787018661@qq.com>
2023-02-25 21:16:51 +0800

    [feat] 增加全局异常处理

---
+++ src/error/globalException.filter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { RuntimeException } from '@nestjs/core/errors/exceptions/runtime.exception';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      code: status,
+      message: (exception as RuntimeException).message || 'Internal Error',
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+
+    // ↓ 输出错误信息
+    console.error(`Exception: ${exception}, errorRequestPath: ${request.url}`);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './error/globalException.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // 全局添加 "api"前缀
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(6006);
 }
 bootstrap();
