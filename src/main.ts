fd9d3735774f93f2381d8e4d3753a4a39ad515f2
keeko <787018661@qq.com>
2023-03-26 10:22:24 +0800

    [feat]  NestJS Interceptor 拦截器封装全局返回体

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './error/globalException.filter'
+import { GlobalExceptionFilter } from './error/globalException.filter';
+import { TransformResponseInterceptor } from './interceptor/transformResponse.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // 全局添加 "api"前缀
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new TransformResponseInterceptor()); // 全局res封装
   await app.listen(7158);
 }
 bootstrap();
